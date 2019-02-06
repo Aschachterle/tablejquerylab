5e4ac0ee80074e060243b78ce9cb3f0e40a9915a
Adam <adam.schachterle@gmail.com>
2019-02-05 19:19:43 -0500

    added popup function

--- script.js
+++ script.js
@@ -1,39 +1,61 @@
 "use strict";
 $(document).ready(function() {
     let divID = null;
-    $(`.circle`).click(function () {
+    let name = null;
+    let phone = null;
+    let people = null;
+    $(document).on("click", ".available", function(e) {
         divID = $(this).attr("id");
-        if ($(`#${divID}`).hasClass(`reserved`)) {
-            alert(`table already taken`);
-        } else {
-            modalbox();
-        }
-        console.log(divID)
-        $(`.tabnum`).html(`Table Number: ${divID}`);
-    });
+        modalbox();
+        $(`.tabnum`).text(`Table Number: ${divID}`);
+    })
+    
     $('.save').click(function() {
-        console.log(divID)
         $(`#${divID}`).addClass(`reserved`);
+        $(`#${divID}`).removeClass(`available`);
         $(`#${divID}`).css(`cursor`,`not-allowed`);
+        name = $(`#name`).val();
+        phone = $(`#phone`).val();
+        people = $(`#people`).val();
+        $(`#${divID}`).attr(`name`, `${name}`);
+        $(`#${divID}`).attr(`phone`, `${phone}`);
+        $(`#${divID}`).attr(`people`, `${people}`);
 
         closeModal();
     })
+    
+    $(`.close`).click(function() {
+        closeModal()
+    })
+
     function modalbox() { 
         $(".modal").toggle(function (e) {   
           let dataModal = $(this).attr("data-modal");
           $("#" + dataModal).css({ "display": "block" });
         })
     }
-    $(`.close`).click(function() {
-        closeModal()
-    })
     function closeModal() {
         $(".modal").toggle(function (e) {   
             let dataModal = $(this).attr("data-modal");
-            $("#" + dataModal).css({ "display": "none" });
+            $("#" + dataModal).css({ "display": "block" });
     })
     }
-    
+    $(document).on("mouseenter", `.reserved`, function(e) {
+
+        $(e.target).append(`
+        <div class="popup">
+        Name: ${$(e.target).attr("name")}
+        Phone: ${$(e.target).attr("phone")}
+        People: ${$(e.target).attr("people")}
+        </div>
+        `);
+        $(".popup").css("display", "flex");
+    });
+    $(document).on("mouseleave", ".reserved", function(e) {
+        $(".popup").css("display", "none");
+        $('.popup').remove();
+    });
 })
+
 
 
